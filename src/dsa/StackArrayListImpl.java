6738883b4af5962339b3186226f3e1b3ddb6445d
Harsh Mangal <harsh.mangal1108@gmail.com>
2022-08-18 15:52:39 +0530

    Stack Complete

---
+++ src/dsa/StackArrayListImpl.java
@@ -0,0 +1,51 @@
+package dsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackArrayListImpl {
+	static class Stack {
+		List<Integer> list = new ArrayList();
+
+		public boolean isEmpty() {
+			return list.size() == 0;
+		}
+
+		public void push(int data) {
+			list.add(data);
+		}
+
+		
+		public int pop() {
+			if (list.isEmpty()) {
+				return -1;
+			}
+			int top = list.get(list.size() - 1);
+			list.remove(list.size() - 1);
+			return top;
+		}
+
+		
+		public int peek() {
+			if (list.isEmpty()) {
+				return -1;
+			}
+			return list.get(list.size() - 1);
+		}
+	}
+
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+
+		while (!st.isEmpty()) {
+			System.out.println(st.peek());
+			st.pop();
+		}
+
+	}
+
+}
---
+++ src/practice/Stack01.java
@@ -0,0 +1,59 @@
+package practice;
+
+import java.util.Stack;
+
+public class Stack01 {
+	private static void pushAtBottomRecursive(int data, Stack<Integer> s) {
+		if (s.isEmpty()) {
+			s.push(data);
+			return;
+		}
+
+		int top = s.pop();
+		pushAtBottomRecursive(data, s);
+		s.push(top);
+
+	}
+
+	private static void pushAtBottomIterative(Integer data, Stack<Integer> st) {
+		Stack<Integer> tempSt = new Stack<>();
+		while (!st.isEmpty()) {
+			int top = st.pop();
+			tempSt.push(top);
+		}
+		st.push(data);
+		while (!tempSt.isEmpty()) {
+			int top = tempSt.pop();
+			st.push(top);
+		}
+
+	}
+
+	private static void reverseAStack(Stack<Integer> s) {
+		if (s.isEmpty()) {
+			return;
+		}
+		int top = s.pop();
+		reverseAStack(s);
+		pushAtBottomRecursive(top, s);
+	}
+
+	public static void main(String[] args) {
+		// Insertion in bottom of stack.
+		Stack<Integer> st = new Stack<>();
+		Integer data = 12312;
+		st.push(1);
+		st.push(2);
+		st.push(3);
+
+//		pushAtBottomRecursive(data, st);
+//		pushAtBottomIterative(data, st);
+		reverseAStack(st);
+		while (!st.isEmpty()) {
+			System.out.println(st.peek());
+			st.pop();
+		}
+
+	}
+
+}
